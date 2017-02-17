39d94a0a4ce7a7a1d651306cffa9af27c3ddca01
Ann Caplin <ann.caplin@gmail.com>
2017-02-16 21:07:56 -0500

    Update EC

--- 03KnightsTour/OptimizedAlgorithm.java
+++ 03KnightsTour/OptimizedAlgorithm.java
@@ -1,9 +1,13 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class OptimizedAlgorithm{
     int[][] board;
     int[][][] branchBoard;
+    boolean solveCalled = false;
     public OptimizedAlgorithm(int rows, int cols){
 	board = new int[rows][cols];
-	branchBoard = new int[rows][cols];
+	branchBoard = new int[rows][cols][];
 	initBranches();
     }
     private void initBranches(){
@@ -18,19 +22,45 @@ public class OptimizedAlgorithm{
 			    (i + x < branchBoard.length && i + x >= 0 &&
 			     j + y < branchBoard[0].length && j + y >= 0)){
 			    locns[len] = (i+x)*board[0].length + (j+y);
+				//System.out.println(getXVal(locns[len]) + "," + getYVal(locns[len]));
 			    len++;
 			}
 		    }
 		}
 		int[] temp = new int[len];
+		for (int x = 0; x < len; x++){
+			temp[x] = locns[x];
+		}
 		branchBoard[i][j] = temp;
 	    }
 	}
     }
 
-    public class MyComparator implements Comparator<Integer>{
-	@Override
-	public int compare(int i1, int i2){
+        public String toString(){
+	String total = "";
+	if (solveCalled){
+	    for (int i = 0; i < board.length; i++){
+		for (int j = 0; j < board[0].length; j++){
+		    if (board[i][j]/10 == 0){
+			total += " ";
+		    }
+		    total += board[i][j] + " ";
+		}
+		total += "\n";
+	    }
+	}else{
+	    for (int i = 0; i < board.length; i++){
+		for (int j = 0; j < board[0].length; j++){
+		    total += "-";
+		}
+		total += "\n";
+	    }
+        }
+	return total;
+    }
+    
+    class MyComparator implements Comparator<Integer>{
+	public int compare(Integer i1,  Integer i2){
 	    return branchBoard[getXVal(i1)][getYVal(i1)].length - branchBoard[getXVal(i2)][getYVal(i2)].length;
 	}
     }
@@ -44,18 +74,55 @@ public class OptimizedAlgorithm{
     }
 
 
-    public void solve(int row, int col, int level){
+    public void solve(){
+	for (int i = 0; i < board.length; i++){
+	    for (int j = 0; j < board[0].length; j++){
+		if (solve(i, j, 1)){
+			solveCalled = true;
+		    return;
+		}
+	    }
+	}
+    }
+    
+    public boolean solve(int row, int col, int level){
 	board[row][col] = level;
 	if (level >= board.length * board[0].length){
 	    return true;
 	}
-        Arrays.sort(branchBoard[row][col], new MyComparator());
+        //Arrays.sort(branchBoard[row][col], new MyComparator());
+		sort(branchBoard[row][col]);
 	for (int i : branchBoard[row][col]){
-	    if (solve(getXVal(i), getYVal(i), level+1){
+		//System.out.println(getXVal(i) + " " + getYVal(i));
+	    if (board[getXVal(i)][getYVal(i)] == 0 && solve(getXVal(i), getYVal(i), level+1)){
 		    return true;
 		}
 	}
 	    board[row][col] = 0;
 	    return false;
     }
+
+	public void sort(int[] arr){
+		int mindex = 0;
+		int index = 0;
+		while (index < arr.length){
+			mindex = index;
+			for (int i = index; i < arr.length; i++){
+				if (branchBoard[getXVal(arr[i])][getYVal(arr[i])].length <
+					branchBoard[getXVal(arr[mindex])][getYVal(arr[mindex])].length){
+					mindex = i;
+				}
+			}
+			int temp = arr[mindex];
+			arr[mindex] = arr[index];
+			arr[index] = temp;
+			index++;
+		}
+	}
+	
+	public static void main(String[] args){
+	    OptimizedAlgorithm oa = new OptimizedAlgorithm(8, 8);
+	    oa.solve();
+	    System.out.println(oa);
+	}
 }
