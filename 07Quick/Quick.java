27286825a108e7c49bcd1e3cf859119e6fbf1443
Ann Caplin <ann.caplin@gmail.com>
2017-03-19 19:38:04 -0400

    Update & test

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -7,11 +7,11 @@ public class Quick{
 	for (int i = 0; i < ary.length; i++){
 	    int index = part(ary, start, end);
 	    if (index > k){
-		end = index-1;
+	    	end = index-1;
 	    }else if(index < k){
-		start = index+1;
+	    	start = index+1;
 	    }else{
-		return ary[index];
+	    	return ary[index];
 	    }
        
 	}
@@ -20,44 +20,47 @@ public class Quick{
     public static int part(int[] ary, int start, int end){
 	//System.out.println("START: " + start + " END: " + end);
 	//System.out.println(makeString(ary));
+    if (start >= end){
+    	return start;
+    }
 	int random = start + (int)(Math.random()*(end-start+1));
+	//System.out.println("\tRDM: " + random + " : " + start + " - " + end);
 	int pivot = ary[random];
 	int t = ary[start];
 	ary[start] = pivot;
 	ary[random] = t;
 	int s = start+1;
 	int e = end;
-	//	System.out.println(makeString(ary));
-	while (e > s){
-	    while (e >= 0 && ary[e] > pivot){
-		e--;
-	    }
-	    while (s < ary.length && ary[s] < pivot){
-		s++;
-	    }
-	    if (s < e){
-	    int temp = ary[s];
-	    ary[s] = ary[e];
-	    ary[e] = temp;
-	    
+	//System.out.println(makeString(ary, start, end));
+	//boolean decreaseE = false;
+	while (e >= s){
+		if (ary[s] > pivot && ary[e] < pivot){
+			int temp = ary[s];
+		    ary[s] = ary[e];
+		    ary[e] = temp;
+		    s++;
+		    e--;
+		  //  decreaseE = true;
+		}else if (ary[s] > pivot){
+			e--;
+		//	decreaseE = true;
+		}else{
+			s++;
+		}
 	 //  System.out.println("start: " + s + " end: " + e + "::" + makeString(ary));
-	    }
 	}
-	int total = start;
-	for (int i = start; i <= end; i++){
-	   if (ary[i] < pivot){
-		total++;
-	}
-		
-	}
+	/*if (!decreaseE){
+		e++;
+	}*/
 	//ary[s] = pivot;
 	//System.out.println(makeString(ary));
-	int t2 = ary[total];
-	ary[total] = ary[start];
-	ary[start] = t2;
-	//System.out.println("\t" + pivot + ": " + total + ": " + makeString(ary, start, end));
+	int t2 = ary[start];
+	//System.out.println(e);
+	ary[start] = ary[e];
+	ary[e] = t2;
+	//System.out.println("\t" + pivot + ": " + (e) + ": " + makeString(ary, start, end));
 	//ary[s-1] = pivot;
-	return total;
+	return e;
     }
    
     public static String makeString(int[] ary, int s, int e){
@@ -68,15 +71,16 @@ public class Quick{
 	return total;
     }
 
-   public static void main(String[] args){
+   /*public static void main(String[] args){
 	//int[] arr = {1, 3, 2, 5, 4, 0};
-	//System.out.println(kthsmall(arr, 5));
+	//System.out.println(kthsmallest(arr, 5));
 	//should be 3
-	/*int[] ary = {2, 10, 15, 23, 0, 5};
+	int[] ary = {2, 10, 15, 23, 23, 23, 0, 5, 5, 5, 5, 5};
 	for (int i = 0; i < ary.length; i++){
 	   System.out.println(i + " " + quickselect(ary, i));
-	}*/
-	for (int x = 0; x < 100; x++){
+	}
+	System.out.println(quickselect(ary, 2));
+	for (int x = 0; x < 1000; x++){
 	int random = (int)(1000*(Math.random()));
 	int[] randomArr = new int[random];
 	int range = (int)(1000*(Math.random()));
@@ -89,11 +93,12 @@ public class Quick{
 	for (int i = 0; i < randomArr.length; i++){
 	  copyRandom[i] = randomArr[i];
 	}
-	System.out.println(makeString(copyRandom, 0, copyRandom.length-1) + "\n");
+	//System.out.println(makeString(copyRandom, 0, copyRandom.length-1) + "\n");
 	Arrays.sort(randomArr);
 	if (randomArr[index] != quickselect(copyRandom, index)){
  	   System.out.println("error");
 	}
 	}
-    }
+    }*/
 }
+
