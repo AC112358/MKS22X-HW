5acf7af058f43b72b9706ad49e3adae8526b7e08
AC112358 <ann.caplin@gmail.com>
2017-03-24 12:09:36 -0400

    Update merge

--- 08Merge/Merge.java
+++ 08Merge/Merge.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class Merge{
     public static void mergesort(int[] ary){
 	mergesort(ary, 0, ary.length-1);
@@ -14,7 +15,8 @@ public class Merge{
     public static void interleave(int[] ary, int start, int mid, int end){
 	int s = start;
 	int avg = mid+1;
-	while (start <= mid && avg <= end){
+	//	System.out.println("**"+mid+ " - " + end + "**");
+	while (start < avg && avg <= end){
 	   
 	    if (ary[start] > ary[avg]){
 		int temp = ary[start];
@@ -27,10 +29,13 @@ public class Merge{
 	    }
 	    else if (ary[start] < ary[avg]){
 		start++;
+	    }else{
+		start++;
+		avg++;
 	    }  
 	    
 	    printArray(ary, s, end);
-	    System.out.println("\t" + start + " " + avg);
+	    //  System.out.println("\t" + start +  " " + avg);
 	}
     }
 
@@ -47,9 +52,34 @@ public class Merge{
 	}
 	System.out.println();
     }
-    public static void main(String[] args){
+    /* public static void main(String[] args){
 	int[] a = {1, 3, 8, 0, 4, 5, 7, 8, 9};
 	mergesort(a);
 	printArray(a);
+	}*/
+    public static void main(String[] args){
+    for (int x = 0; x < 1000; x++){
+	int random = (int)(1000*(Math.random()));
+	int[] randomArr = new int[random];
+	int range = (int)(1000*(Math.random()));
+	for (int i = 0; i < randomArr.length; i++){
+	   randomArr[i] = (int)(range*Math.random());
+	}
+	int index = (int)(Math.random()*randomArr.length);
+	int elmt = randomArr[index];
+	int[] copyRandom = new int[randomArr.length];
+	for (int i = 0; i < randomArr.length; i++){
+	  copyRandom[i] = randomArr[i];
+	}
+	//System.out.println(makeString(copyRandom, 0, copyRandom.length-1) + "\n");
+	Arrays.sort(randomArr);
+	mergesort(copyRandom);
+	for (int i = 0; i < randomArr.length; i++){
+	    if (randomArr[i] != copyRandom[i]){
+		System.out.println("error\n\t");
+		//   printArray(copyRandom);
+	    }
+	}
+       }
     }
 }
