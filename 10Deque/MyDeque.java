d1de2426916fb343aa735b8e1473d82757f1db96
AC112358 <ann.caplin@gmail.com>
2017-04-07 15:33:37 -0400

    Update MyDeque

---
+++ 10Deque/MyDeque.java
@@ -0,0 +1,90 @@
+import java.util.NoSuchElementException;
+
+public class MyDeque{
+    String[] data;
+    int first;
+    int last;
+    int size;
+    public MyDeque(){
+	data = new String[3];
+	first = 0;
+	last = 0;
+	size = 0;
+    }
+    public void addFirst(String toAdd){
+	if (toAdd == null){
+	    throw new NullPointerException();
+	}
+	if (first == last){
+	    grow();
+	}
+	first--;
+	data[first] = toAdd;
+	size++;
+    }
+    public void grow(){
+	String[] newArr = new String[data.length*2];
+	for (int i = first; i < size+first-1; i++){
+	    newArr[i-first] = data[i%data.length];
+	}
+	data = newArr;
+    }
+    public void addLast(String toAdd){
+       if (toAdd == null){
+	    throw new NullPointerException();
+	}
+	if (first == last){
+	    grow();
+	}
+	last++;
+	data[last] = toAdd;
+	size++;
+    }
+    
+    public String removeFirst(){
+	if (size == 0){
+	    throw new NoSuchElementException();
+	}
+	String temp = data[first];
+	first = (first+1)%data.length;
+	size--;
+	return temp;
+    }
+    public String removeLast(){
+	if (size == 0){
+	    throw new NoSuchElementException();
+	}
+	String temp = data[last];
+	last = (last-1)%data.length;
+	return temp;
+    }
+    public String getFirst(){
+	if (size == 0){
+	    throw new NoSuchElementException();
+	}
+	return data[first];
+    }
+    public String getLast(){
+	if (size == 0){
+	    throw new NoSuchElementException();
+	}
+	return data[last];
+    }
+    public String toString(){
+	String total = "";
+	for (int i = first; i < size+first-1; i++){
+	    total += data[i%data.length] + " ";
+	}
+	return total;
+    }
+    public static void main(String[] args){
+	MyDeque d = new MyDeque();
+	d.addFirst("a");
+	System.out.println(d);
+	d.addLast("b");
+	System.out.println(d);
+	d.addFirst("c");
+	System.out.println(d);
+	
+    }
+}
