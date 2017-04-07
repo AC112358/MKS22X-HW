9a60f2b69cf2e291d4e08c574d2e6cbe0ca27dcb
Ann Caplin <Ann Caplin>
2017-04-07 17:31:45 -0400

    Update MyDeque

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -7,7 +7,7 @@ public class MyDeque{
     int size;
     public MyDeque(){
 	data = new String[3];
-	first = 0;
+	first = 1;
 	last = 0;
 	size = 0;
     }
@@ -15,16 +15,16 @@ public class MyDeque{
 	if (toAdd == null){
 	    throw new NullPointerException();
 	}
-	if (first == last){
+	if (size >= data.length - 1){
 	    grow();
 	}
-	first--;
+	first = (first-1+data.length)%data.length;
 	data[first] = toAdd;
 	size++;
     }
     public void grow(){
 	String[] newArr = new String[data.length*2];
-	for (int i = first; i < size+first-1; i++){
+	for (int i = first; i < size+first; i++){
 	    newArr[i-first] = data[i%data.length];
 	}
 	data = newArr;
@@ -33,10 +33,10 @@ public class MyDeque{
        if (toAdd == null){
 	    throw new NullPointerException();
 	}
-	if (first == last){
+	if (size >= data.length-1){
 	    grow();
 	}
-	last++;
+	last = (last+1)%data.length;
 	data[last] = toAdd;
 	size++;
     }
@@ -55,7 +55,8 @@ public class MyDeque{
 	    throw new NoSuchElementException();
 	}
 	String temp = data[last];
-	last = (last-1)%data.length;
+	last = (last-1+data.length)%data.length;
+	size--;
 	return temp;
     }
     public String getFirst(){
@@ -72,19 +73,41 @@ public class MyDeque{
     }
     public String toString(){
 	String total = "";
-	for (int i = first; i < size+first-1; i++){
-	    total += data[i%data.length] + " ";
+	for (int i = 0; i < data.length; i++){
+	    System.out.print(data[i] + " ");
+	}
+	System.out.println();
+	for (int i = first; i < size + first; i++){
+	    total += data[i % data.length] + " ";
 	}
 	return total;
     }
+    /*
     public static void main(String[] args){
 	MyDeque d = new MyDeque();
 	d.addFirst("a");
-	System.out.println(d);
+	System.out.println("Add a to front: " + d);
 	d.addLast("b");
-	System.out.println(d);
+	System.out.println("Add b to back: " + d);
 	d.addFirst("c");
-	System.out.println(d);
-	
-    }
+	System.out.println("Add c to front: " + d);
+	System.out.println("First element: " + d.getFirst());
+	System.out.println("Last element: " + d.getLast());
+	d.removeLast();
+	System.out.println("Remove last: " + d);
+	System.out.println("First element: " + d.getFirst());
+	System.out.println("Last element: " + d.getLast());
+       	d.removeFirst();
+	System.out.println("Remove first: " + d);
+	System.out.println("First element: " + d.getFirst());
+	System.out.println("Last element: " + d.getLast());
+	d.removeFirst();
+	System.out.println("Remove first: " + d);
+	System.out.println("First element: " + d.getFirst());
+	System.out.println("Last element: " + d.getLast());
+	d.removeLast();
+	System.out.println("Remove last: " + d);
+	System.out.println("First element: " + d.getFirst());
+	System.out.println("Last element: " + d.getLast());
+	}*/
 }
