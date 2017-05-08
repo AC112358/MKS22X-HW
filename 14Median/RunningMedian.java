cf8df3659ac6ad5e6643f22771f781373c0927a8
AC112358 <ann.caplin@gmail.com>
2017-05-08 15:32:03 -0400

    Update running median

---
+++ 14Median/RunningMedian.java
@@ -0,0 +1,49 @@
+public class RunningMedian{
+    private double median;
+    private MyHeap minHeap;
+    private MyHeap maxHeap;
+    public RunningMedian(){
+	median = 0;
+	minHeap = new MyHeap(false);
+	maxHeap = new MyHeap(true);
+    }
+    
+    public void add(int toAdd){
+	if (toAdd > median){
+	    minHeap.add(toAdd);
+	}
+	else{
+	    maxHeap.add(toAdd);
+	}
+	if (minHeap.getSize() == maxHeap.getSize()){
+	    median = (minHeap.peek() + maxHeap.peek())/2.;
+	}
+	else if (minHeap.getSize() - maxHeap.getSize() == 1){
+	    median = minHeap.peek();
+	}
+	else if (maxHeap.getSize() - minHeap.getSize() == 1){
+	    median = maxHeap.peek();
+	}
+	else if (minHeap.getSize() - maxHeap.getSize() > 2){
+	    maxHeap.add(minHeap.remove());
+	}
+	else if (maxHeap.getSize() - minHeap.getSize() > 2){
+	    minHeap.add(maxHeap.remove());
+	}
+    }
+    
+    public double getMedian(){
+	return median;
+    }
+
+
+
+    public static void main(String[] args){
+	int[] list = {14, 12, 19, 20, 124, 124, 56, 23, 123, 575, 7};
+	RunningMedian med = new RunningMedian();
+	for (int i = 0; i < list.length; i++){
+	    med.add(list[i]);
+	    System.out.println(med.getMedian());
+	}
+    }
+}
